51c73cbb73fdaf9ae84888a636494cba447be2ee
Sara-M <sara.monintja@gmail.com>
2022-06-27 14:25:26 +0800

    added changes on html and js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // when i click on the search button 
 const searchForm = document.getElementById('search-form');
 const inputSearch = document.getElementById('input-search');
+const currentDayCity = document.getElementById('current-day-city')
+const currentDayTemp = document.getElementById('current-day-temp')
+const currentDayWind = document.getElementById('current-day-wind')
+const currentDayHumidity = document.getElementById('current-day-humidity')
 
 const apiKey = '3cac805e6362cc29d52eeef14b2bcc3a';
 
+function getOneCallApi (lon, lat, datetime){
+
+    return fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid={apiKey}`)
+        .then(function(response){
+            return response.json()
+        })
+       
+}
+
 function getWeatherData(city) {
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(function (response){
         return response.json();
     })
@@ -14,6 +27,7 @@ function getWeatherData(city) {
     
         console.log(currentWeather);
 
+        returns.getOneCallApi(currentWeather.coord.lon, currentWeather.coord.lat, currentWeather.dt)
     })
 
 }
@@ -23,25 +37,35 @@ searchForm.addEventListener('submit', function(event){
  // get user input
     const userInput = inputSearch.value;
 
-// send req to weatherdashboard api, 
+    // send req to weatherdashboard api, 
 
-// fetch weather data based on city name
+    // fetch weather data based on city name
 
-getWeatherData(userInput)
+    getWeatherData(userInput)
 
+        .then(function(weatherData){
+    
+        console.log(weatherData);
+
+        // once we got the data, 
+        // populate the data into the dom
+
+        // current
+        const datetime = moment(weatherData.current.dt, 'X').format("YYYY-MM-DD");
+        console.log(datetime);
+
+        currentDayCity.innerHTML = `${userInput} ${datetime} icon`
+        currentDayHumidity.textContent = weatherData.current.humidity;
+        currentDayTemp.textContent = weatherData.current.temp + 'K';
+        currentDayWind.textContent = weatherData.current.wind_speed + "kmh";
+    })
 
 });
-  
 
- 
+localStorage.setItem('city', 'userInput');
 
-
-
-
-
-// once we got the data, 
-// ppopulate the data into the dom
 
 // store the city name into localstorage
+
 // render the history in the search list
 
